66e610d3667fd7cb227ffdae0596407ac66ac4f5
Pierre Janineh <janinehpierre@gmail.com>
2021-03-01 18:22:11 +0200

    Initial Commit

---
+++ iOSDatingApp/iOSDatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDatingAppApp.swift
+//  iOSDatingApp
+//
+//  Created by Pierre Janineh on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSDatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
